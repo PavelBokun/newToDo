78050db3e72bbb517fd035f420b4000f7cb0e579
PavelBokun <pavel.snabteh@gmail.com>
2025-02-02 19:31:06 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { TodolistItem } from './TodolistItem'
+
+   export const App=()=> {
+  return (
+      <div className="app">
+      <TodolistItem title="What to learn" />
+        <TodolistItem title="Songs"/>
+        <TodolistItem title="Books" />
+      </div>
+  )
+}
+
