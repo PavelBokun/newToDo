f70e089b03a2ab8f7a3176dddc5656c662144ada
PavelBokun <pavel.snabteh@gmail.com>
2025-02-05 22:30:25 +0300

    0502

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import "./App.css";
+import { Button } from "./dd";
+import { Filter } from "./filter";
 import { TodolistItem } from "./TodolistItem";
+import { Car } from "./topcar";
 
 export const App = () => {
   const tasks1 = [
@@ -18,11 +21,21 @@ export const App = () => {
     { id: 4, title: "Redux", isDone: false },
   ];
   
+  const topCars = [
+    {id:1, manufacturer:'BMW', model:'m5cs'},
+    { id:2, manufacturer:'Mercedes', model:'e63s'},
+    {id:3, manufacturer:'Audi', model:'rs6'}
+  ]
+
+  
 
   return (
     <div className="app">
       <TodolistItem title="What to learn" tasks={tasks1} date={""} />
       <TodolistItem title="Songs" tasks={tasks2} />
+      <Button />
+      <Car car={topCars}/>
+      <Filter />
     </div>
   );
 };
---
+++ src/filter.tsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+
+type Money = {
+  banknots: string;
+  value: number;
+  number: string;
+};
+
+type Filter = 'rub'|'dol'|'all'
+
+export const Filter = () => {
+  const [money, setMoney] = useState<Money[]>([
+    { banknots: "Dollars", value: 100, number: " a1234567890" },
+    { banknots: "Dollars", value: 50, number: " z1234567890" },
+    { banknots: "RUBLS", value: 100, number: " w1234567890" },
+    { banknots: "Dollars", value: 100, number: " e1234567890" },
+    { banknots: "Dollars", value: 50, number: " c1234567890" },
+    { banknots: "RUBLS", value: 100, number: " r1234567890" },
+    { banknots: "Dollars", value: 50, number: " x1234567890" },
+    { banknots: "RUBLS", value: 50, number: " v1234567890" },
+  ]);
+
+const [filter, setFilter] = useState<Filter>("all");
+
+let curentmoney =money
+if(filter === "dol")
+{curentmoney=money.filter((b) => b.banknots === "Dollars")}
+if(filter === "rub")
+    {curentmoney=money.filter((b) => b.banknots === "RUBLS")}
+
+
+
+
+  
+
+  const finc1 = (name:Filter) => {
+    setFilter (name)
+  };
+
+  return (
+    <>
+      {curentmoney.length === 0 ? (
+        <span>No money</span>
+      ) : (
+        <ul>
+          {curentmoney.map(
+            (
+              c,
+              index // Убираем any и добавляем index для ключа
+            ) => (
+              <li key={index}>
+                <span>{c.banknots}</span> {/* Исправлено на banknots */}
+                <span>{c.value}</span> {/* Добавлено отображение value */}
+                <span>{c.number}</span> {/* Исправлено на number */}
+              </li>
+            )
+          )}
+        </ul>
+      )}
+      <div>
+        <button onClick={() => finc1("dol")}>dol</button>
+        <button onClick={() => finc1("rub")}>rub</button>
+        <button onClick={() => finc1("all")}>all</button>
+      </div>
+    </>
+  );
+};
