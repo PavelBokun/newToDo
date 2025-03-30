91dcc4e95fed61b8a5accfa88f19a087489ed01b
PavelBokun <pavel.snabteh@gmail.com>
2025-03-30 23:02:22 +0300

    30031

--- src/App.tsx
+++ src/App.tsx
@@ -69,7 +69,7 @@ export const App = () => {
 
 let todolists: Array<TodoListType>=[
   {id:v1(),title:'What to learn', filter:'active'},
-  {id:v1(),title:'What to learn2', filter:'completed'}
+  {id:v1(),title:'What to learn2', filter:'completed'},
   {id:v1(),title:'What to learn2', filter:'completed'}
 
 ]
